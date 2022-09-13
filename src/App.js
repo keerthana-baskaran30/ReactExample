9593c2b12ec5c63f9f09f88b444751f7e245258c
Keerthana-30 <71180623+Keerthana-30@users.noreply.github.com>
2022-09-13 10:20:33 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
-import Greet from './components/Home'
+import Greet,{Home,ExpenseEntryItem, Counter} from './components/Home'
+
+
+const name = "Grape Juice"
+const amount = 30.00
+const spendDate = new Date("2020-10-10")
+const category = "Food"
+
+
+
+const item = {
+  id: 1, 
+  name : "Grape Juice", 
+  amount : 30.5, 
+  spendDate: new Date("2020-10-10"), 
+  category: "Food" 
+}
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+        <Greet name = "Keerthana"/>
+        <Home/> 
+      {/* <ExpenseEntryItem 
+               name={name} 
+               amount={amount} 
+               spendDate={spendDate} 
+               category={category} />, */}
+
+      <ExpenseEntryItem item = {item}/>
+      <button onClick = {Counter}> click</button>
+      <Counter></Counter>
+     
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,70 @@
 import React from 'react'
 
 
-function Greet(){
-    return <h1>kiki</h1>
+export default function Greet(value){
+    const id = "greet"
+    return (<div>
+                <h1 id = {id} className = "greet class">Hi {value.name}</h1>
+                <Address/>
+            </div>     
+        );
 }
 
-export default Greet
+// export default function Greet(value){
+//     return (<div>
+//                 <h1>Hi {value.name}</h1>
+//                 <Address/>
+//             </div>   
+//             <h1>qwert</h1> 
+//         ); Cant return values like this put h1 inside a div or send it as array with a key attribute
+// }
+
+export function Home(){
+    const jsx = <h1 id ="jsx">Hello</h1>;
+    console.log(jsx)
+    return jsx
+}
+
+// using React Fragment to send multiple elements
+export function Address(){
+    return (
+        // <React.Fragment>
+        //     <h1>wedrftgvdk</h1>
+        //     <h2>dertgfrtgvc</h2>
+        // </React.Fragment>
+        <>
+            <h1>wedrftgvdk</h1>
+            <h2>dertgfrtgvc</h2>
+        </>
+    )
+}
+
+
+export class ExpenseEntryItem extends React.Component {
+    constructor(props) {
+       super(props);
+    }
+    render() {
+       return (
+          <div>
+             <div><b>Item:</b> <em>{this.props.item.name}</em></div>
+             <div><b>Amount:</b> <em>{this.props.item.amount}</em></div>
+             <div><b>Spend Date:</b> <em>{this.props.item.spendDate.toString()}</em></div>
+             <div><b>Category:</b> <em>{this.props.item.category}</em></div>
+          </div>
+       );
+    }
+}
+ExpenseEntryItem.defaultProps = {
+    name:"qaz",
+    amount:"ygd",
+};
+
+let counter = 0;
+export function Counter(){
+    counter++;
+    console.log("counter", counter);
+    return <h1>{counter}</h1>;
+}
+// export default Greet
+
